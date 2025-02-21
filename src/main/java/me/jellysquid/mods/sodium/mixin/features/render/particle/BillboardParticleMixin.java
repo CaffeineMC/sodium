0373cc60369e9951996d97e86a82a604c37742e0
JellySquid <jellysquid@pm.me>
2025-02-20 23:20:29 -0600

    Skip particle rendering optimizations for incompatible mods (#2827)
    Cherry-pick of e69c9abf.

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/render/particle/BillboardParticleMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/render/particle/BillboardParticleMixin.java
@@ -1,7 +1,7 @@
 package me.jellysquid.mods.sodium.mixin.features.render.particle;
 
+import me.jellysquid.mods.sodium.client.render.vertex.VertexConsumerUtils;
 import net.caffeinemc.mods.sodium.api.vertex.format.common.ParticleVertex;
-import net.caffeinemc.mods.sodium.api.vertex.buffer.VertexBufferWriter;
 import net.caffeinemc.mods.sodium.api.util.ColorABGR;
 import net.minecraft.client.particle.BillboardParticle;
 import net.minecraft.client.particle.Particle;
@@ -13,9 +13,11 @@ import net.minecraft.util.math.Vec3d;
 import org.joml.Quaternionf;
 import org.lwjgl.system.MemoryStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BillboardParticle.class)
 public abstract class BillboardParticleMixin extends Particle {
@@ -42,8 +44,16 @@ public abstract class BillboardParticleMixin extends Particle {
      * @reason Optimize function
      * @author JellySquid
      */
-    @Overwrite
-    public void buildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
+    @Inject(method = "buildGeometry", at = @At("HEAD"), cancellable = true)
+    public void buildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta, CallbackInfo ci) {
+        final var writer = VertexConsumerUtils.convertOrLog(vertexConsumer);
+
+        if (writer == null) {
+            return;
+        }
+
+        ci.cancel();
+
         Vec3d vec3d = camera.getPos();
 
         float x = (float) (MathHelper.lerp(tickDelta, this.prevPosX, this.x) - vec3d.getX());
@@ -71,22 +81,20 @@ public abstract class BillboardParticleMixin extends Particle {
 
         int color = ColorABGR.pack(this.red , this.green, this.blue, this.alpha);
 
-        var writer = VertexBufferWriter.of(vertexConsumer);
-
         try (MemoryStack stack = MemoryStack.stackPush()) {
             long buffer = stack.nmalloc(4 * ParticleVertex.STRIDE);
             long ptr = buffer;
 
-            writeVertex(ptr, quaternion,-1.0F, -1.0F, x, y, z, maxU, maxV, color, light, size);
+            sodium$writeVertex(ptr, quaternion,-1.0F, -1.0F, x, y, z, maxU, maxV, color, light, size);
             ptr += ParticleVertex.STRIDE;
 
-            writeVertex(ptr, quaternion,-1.0F, 1.0F, x, y, z, maxU, minV, color, light, size);
+            sodium$writeVertex(ptr, quaternion,-1.0F, 1.0F, x, y, z, maxU, minV, color, light, size);
             ptr += ParticleVertex.STRIDE;
 
-            writeVertex(ptr, quaternion,1.0F, 1.0F, x, y, z, minU, minV, color, light, size);
+            sodium$writeVertex(ptr, quaternion,1.0F, 1.0F, x, y, z, minU, minV, color, light, size);
             ptr += ParticleVertex.STRIDE;
 
-            writeVertex(ptr, quaternion,1.0F, -1.0F, x, y, z, minU, maxV, color, light, size);
+            sodium$writeVertex(ptr, quaternion,1.0F, -1.0F, x, y, z, minU, maxV, color, light, size);
             ptr += ParticleVertex.STRIDE;
 
             writer.push(stack, buffer, 4, ParticleVertex.FORMAT);
@@ -96,11 +104,11 @@ public abstract class BillboardParticleMixin extends Particle {
 
     @Unique
     @SuppressWarnings("UnnecessaryLocalVariable")
-    private static void writeVertex(long buffer,
-                                    Quaternionf rotation,
-                                    float posX, float posY,
-                                    float originX, float originY, float originZ,
-                                    float u, float v, int color, int light, float size) {
+    private static void sodium$writeVertex(long buffer,
+                                           Quaternionf rotation,
+                                           float posX, float posY,
+                                           float originX, float originY, float originZ,
+                                           float u, float v, int color, int light, float size) {
         // Quaternion q0 = new Quaternion(rotation);
         float q0x = rotation.x();
         float q0y = rotation.y();
