9aded583eb84c80e815ee5ebb93273368febf364
akirasumeragi699-design <akirasumeragi699@gmail.com>
2025-07-30 13:08:35 +0800

    Update build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    modImplementation(files("libs/BadOptimizations-2.3.0-1.21.6-21.7.jar"))
+    modImplementation(files("libs/entityculling-fabric-1.8.2-mc1.21.6.jar"))
+    modImplementation(files("libs/ferritecore-8.0.0-fabric.jar"))
+    modImplementation(files("libs/iris-fabric-1.9.1+mc1.21.7.jar"))
+    modImplementation(files("libs/lithium-fabric-0.18.0+mc1.21.8.jar"))
+    modImplementation(files("libs/moreculling-fabric-1.21.6-1.4.0-beta.1.jar"))
+}
