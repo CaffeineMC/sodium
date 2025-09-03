22657816aaba8f5009cd600dffe4e835be6dc1c6
Mr-Squid42 <51229478+Mr-Squid42@users.noreply.github.com>
2025-09-03 20:48:32 +0100

    Update ModelQuadOrientation.java

--- common/src/main/java/net/caffeinemc/mods/sodium/client/model/quad/properties/ModelQuadOrientation.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/model/quad/properties/ModelQuadOrientation.java
@@ -7,8 +7,8 @@ import net.fabricmc.fabric.api.renderer.v1.mesh.QuadView;
  * consistent order, eliminating a number of shading issues caused by anisotropy problems.
  */
 public enum ModelQuadOrientation {
-    NORMAL(new int[] { 0, 3, 1, 2 }),
-    FLIP(new int[] { 0, 1, 3, 2 });
+    NORMAL(new int[] { 0, 1, 2, 3}),
+    FLIP(new int[] { 1, 2, 3 , 0});
 
     private final int[] indices;
 
