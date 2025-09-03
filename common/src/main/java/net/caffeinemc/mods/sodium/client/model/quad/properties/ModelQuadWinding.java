a1c4af38eb28f2f2685fa1411ecd5b1b8fd546ad
Mr-Squid42 <51229478+Mr-Squid42@users.noreply.github.com>
2025-09-03 21:07:23 +0100

    Update ModelQuadWinding.java

--- common/src/main/java/net/caffeinemc/mods/sodium/client/model/quad/properties/ModelQuadWinding.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/model/quad/properties/ModelQuadWinding.java
@@ -1,8 +1,8 @@
 package net.caffeinemc.mods.sodium.client.model.quad.properties;
 
 public enum ModelQuadWinding {
-    CLOCKWISE(new int[] { 0, 1, 2, 3}),
-    COUNTERCLOCKWISE(new int[] { 0, 3, 2, 1 });
+    CLOCKWISE(new int[] { 0, 1, 3, 2 }),
+    COUNTERCLOCKWISE(new int[] { 2, 3, 1, 0 });
 
     private final int[] indices;
 
