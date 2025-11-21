c8106e97540c585bc032a3bdef0a52ca5826b5b3
douira <douira100@gmail.com>
2025-11-21 03:15:06 +0100

    show reduced debug info by default even in full mode, except for in dev environments
    show different debug infos depending on the profile
    
    use more robust mixin to insert sodium's debug option as in F3 by default
    
    Show our debug information by default on the F3 screen

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager.java
@@ -854,7 +854,7 @@ public class RenderSectionManager {
         return this.sectionByPosition.get(SectionPos.asLong(x, y, z));
     }
 
-    public Collection<String> getDebugStrings() {
+    public Collection<String> getDebugStrings(boolean verbose) {
         List<String> list = new ArrayList<>();
 
         int count = 0;
@@ -882,18 +882,26 @@ public class RenderSectionManager {
             count++;
         }
 
-        list.add(String.format("Pools: Geometry %d/%d MiB, Index %d/%d MiB (%d buffers)",
-                MathUtil.toMib(geometryDeviceUsed), MathUtil.toMib(geometryDeviceAllocated),
-                MathUtil.toMib(indexDeviceUsed), MathUtil.toMib(indexDeviceAllocated), count));
-        list.add(String.format("Transfer Queue: %s", this.regions.getStagingBuffer().toString()));
+        if (verbose) {
+            list.add(String.format("Pools: Geometry %d/%d MiB, Index %d/%d MiB (%d buffers)",
+                    MathUtil.toMib(geometryDeviceUsed), MathUtil.toMib(geometryDeviceAllocated),
+                    MathUtil.toMib(indexDeviceUsed), MathUtil.toMib(indexDeviceAllocated), count));
+            list.add(String.format("Transfer Queue: %s", this.regions.getStagingBuffer().toString()));
+        } else {
+            list.add(String.format("Geometry %d/%d MiB, Index %d/%d MiB (#%d), TQ: %s",
+                    MathUtil.toMib(geometryDeviceUsed), MathUtil.toMib(geometryDeviceAllocated),
+                    MathUtil.toMib(indexDeviceUsed), MathUtil.toMib(indexDeviceAllocated), count, this.regions.getStagingBuffer().toString()));
+        }
 
         list.add(String.format("Chunk Builder: Schd=%02d | Busy=%02d (%04d%%) | Total=%02d",
                 this.builder.getScheduledJobCount(), this.builder.getBusyThreadCount(), (int) (this.builder.getBusyFraction(this.lastFrameDuration) * 100), this.builder.getTotalThreadCount())
         );
 
-        list.add(String.format("Tasks: N0=%03d | N1=%03d | Def=%03d, Recv=%03d",
-                this.thisFrameBlockingTasks, this.nextFrameBlockingTasks, this.deferredTasks, this.buildResults.size())
-        );
+        if (verbose) {
+            list.add(String.format("Tasks: N0=%03d | N1=%03d | Def=%03d, Recv=%03d",
+                    this.thisFrameBlockingTasks, this.nextFrameBlockingTasks, this.deferredTasks, this.buildResults.size())
+            );
+        }
 
         if (PlatformRuntimeInformation.getInstance().isDevelopmentEnvironment()) {
             var meshTaskParameters = this.jobDurationEstimator.toString(ChunkBuilderMeshingTask.class);
@@ -909,7 +917,7 @@ public class RenderSectionManager {
         }
 
         if (this.sortBehavior != SortBehavior.OFF) {
-            this.sortTriggering.addDebugStrings(list, this.sortBehavior);
+            this.sortTriggering.addDebugStrings(list, this.sortBehavior, verbose);
         } else {
             list.add("TS OFF");
         }
