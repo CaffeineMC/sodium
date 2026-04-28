royserp/vulkanium

Reset button for options with hold-shift to reset functionality

common/src/main/java/net/caffeinemc/mods/sodium/client/gui/options/control/SliderControl.java
@@ -2,7 +2,6 @@ package net.caffeinemc.mods.sodium.client.gui.options.control;
 
 import com.mojang.blaze3d.platform.cursor.CursorTypes;
 import net.caffeinemc.mods.sodium.client.config.structure.IntegerOption;
-import net.caffeinemc.mods.sodium.client.config.structure.Option;
 import net.caffeinemc.mods.sodium.client.config.structure.StatefulOption;
 import net.caffeinemc.mods.sodium.client.gui.ColorTheme;
 import net.caffeinemc.mods.sodium.client.gui.Colors;
@@ -36,7 +35,7 @@ public class SliderControl implements Control {
         throw new UnsupportedOperationException("Not implemented");
     }
 
-    static class SliderControlElement extends ControlElement {
+    static class SliderControlElement extends StatefulControlElement {
         private static final int THUMB_WIDTH = 2, TRACK_HEIGHT = 1;
 
         private final IntegerOption option;
@@ -55,7 +54,7 @@ public class SliderControl implements Control {
         }
 
         @Override
-        public Option getOption() {
+        public IntegerOption getOption() {
             return this.option;
         }
 
@@ -77,18 +76,18 @@ public class SliderControl implements Control {
 
             int labelWidth = this.font.width(label);
 
+            // render the label first and then the slider to prevent the highlight rect from darkening the slider
+            super.extractRenderState(graphics, mouseX, mouseY, delta);
+
+            if (!this.option.showControl() || this.isResetOverlayActive()) {
+                return;
+            }
+
             boolean drawSlider = isEnabled && (this.hovered || this.isFocused());
             if (drawSlider) {
                 this.contentWidth = sliderWidth + labelWidth;
             } else {
                 this.contentWidth = labelWidth;
-            }
-
-            // render the label first and then the slider to prevent the highlight rect from darkening the slider
-            super.extractRenderState(graphics, mouseX, mouseY, delta);
-
-            if (!this.option.showControl()) {
-                return;
             }
 
             if (drawSlider) {
@@ -153,6 +152,9 @@ public class SliderControl implements Control {
         @Override
         public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
             this.sliderHeld = false;
+
+            if (super.mouseClicked(event, doubleClick)) return true;
+            if (this.isResetOverlayActive()) return false;
 
             if (this.option.isEnabled() && event.button() == 0 && this.isMouseOver(event.x(), event.y())) {
                 if (this.isMouseOverSlider(event.x(), event.y())) {

common/src/main/java/net/caffeinemc/mods/sodium/client/gui/options/control/StatefulControlElement.java
@@ -1,0 +1,44 @@
+package net.caffeinemc.mods.sodium.client.gui.options.control;
+
+import net.caffeinemc.mods.sodium.client.config.structure.StatefulOption;
+import net.caffeinemc.mods.sodium.client.gui.ColorTheme;
+import net.caffeinemc.mods.sodium.client.gui.widgets.ResetButton;
+import net.caffeinemc.mods.sodium.client.util.Dim2i;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.input.MouseButtonEvent;
+
+public abstract class StatefulControlElement extends ControlElement {
+    protected final ResetButton resetButton;
+
+    public StatefulControlElement(AbstractOptionList list, Dim2i dim, ColorTheme theme) {
+        super(list, dim, theme);
+
+        this.resetButton = new ResetButton(this, () -> this.getOption().resetToDefault());
+    }
+
+    public boolean isResetOverlayActive() {
+        return this.resetButton.isActive();
+    }
+
+    public abstract StatefulOption<?> getOption();
+
+    @Override
+    public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float delta) {
+        this.hovered = this.isMouseOver(mouseX, mouseY);
+
+        super.extractRenderState(graphics, mouseX, mouseY, delta);
+
+        this.resetButton.extractRenderState(graphics, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+        return this.resetButton.mouseClicked(event, doubleClick);
+    }
+
+    @Override
+    protected String truncateLabelToFit(String name) {
+        int rightReserve = this.isResetOverlayActive() ? this.resetButton.getWidth() : this.getContentWidth() + 20;
+        return truncateTextToFit(name, this.getWidth() - rightReserve);
+    }
+}

common/src/main/java/net/caffeinemc/mods/sodium/client/gui/widgets/ResetButton.java
@@ -1,0 +1,89 @@
+package net.caffeinemc.mods.sodium.client.gui.widgets;
+
+import com.mojang.blaze3d.platform.cursor.CursorTypes;
+import net.caffeinemc.mods.sodium.client.gui.Layout;
+import net.caffeinemc.mods.sodium.client.util.Dim2i;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ComponentPath;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.navigation.FocusNavigationEvent;
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.Identifier;
+import org.jspecify.annotations.Nullable;
+
+/**
+ * Right-aligned reset overlay shown when the parent row is hovered while SHIFT is held.
+ * It derives is positioning from the parent so it tracks the parent's scroll position automatically.
+ */
+public class ResetButton extends AbstractWidget {
+    private static final Identifier ICON = Identifier.fromNamespaceAndPath("sodium", "textures/gui/reset_button.png");
+    private static final int ICON_SIZE = 10;
+    private static final int COLOR = 0xFFFF8C30;
+
+    private final AbstractWidget parent;
+    private final Runnable action;
+
+    public ResetButton(AbstractWidget parent, Runnable action) {
+        super(new Dim2i(0, 0, Layout.BUTTON_SHORT, 0));
+        this.parent = parent;
+        this.action = action;
+    }
+
+    public static boolean isShiftHeld() {
+        return Minecraft.getInstance().hasShiftDown();
+    }
+
+    public boolean isActive() {
+        return this.parent.isHovered() && isShiftHeld();
+    }
+
+    @Override
+    public int getX() {
+        return this.parent.getLimitX() - this.getWidth();
+    }
+
+    @Override
+    public int getY() {
+        return this.parent.getY();
+    }
+
+    @Override
+    public int getHeight() {
+        return this.parent.getHeight();
+    }
+
+    @Override
+    public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float delta) {
+        if (!this.isActive()) {
+            return;
+        }
+
+        int x = this.getCenterX() - ICON_SIZE / 2;
+        int y = this.getCenterY() - ICON_SIZE / 2;
+
+        graphics.blit(RenderPipelines.GUI_TEXTURED, ICON, x, y, 0, 0, ICON_SIZE, ICON_SIZE, ICON_SIZE, ICON_SIZE, COLOR);
+        graphics.requestCursor(CursorTypes.POINTING_HAND);
+    }
+
+    @Override
+    public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+        if (!isShiftHeld() || event.button() != 0) {
+            return false;
+        }
+
+        if (!this.parent.isMouseOver(event.x(), event.y()) || !this.isMouseOver(event.x(), event.y())) {
+            return false;
+        }
+
+        this.action.run();
+        this.playClickSound();
+        return true;
+    }
+
+    @Override
+    public @Nullable ComponentPath nextFocusPath(FocusNavigationEvent event) {
+        // Only reachable via SHIFT-hover + click.
+        return null;
+    }
+}

CaffeineMC/sodium

common/src/main/java/net/caffeinemc/mods/sodium/client/gui/options/control/SliderControl.java
@@ -2,7 +2,6 @@ package net.caffeinemc.mods.sodium.client.gui.options.control;
 
 import com.mojang.blaze3d.platform.cursor.CursorTypes;
 import net.caffeinemc.mods.sodium.client.config.structure.IntegerOption;
-import net.caffeinemc.mods.sodium.client.config.structure.Option;
 import net.caffeinemc.mods.sodium.client.config.structure.StatefulOption;
 import net.caffeinemc.mods.sodium.client.gui.ColorTheme;
 import net.caffeinemc.mods.sodium.client.gui.Colors;
@@ -36,7 +35,7 @@ public class SliderControl implements Control {
         throw new UnsupportedOperationException("Not implemented");
     }
 
-    static class SliderControlElement extends ControlElement {
+    static class SliderControlElement extends StatefulControlElement {
         private static final int THUMB_WIDTH = 2, TRACK_HEIGHT = 1;
 
         private final IntegerOption option;
@@ -55,7 +54,7 @@ public class SliderControl implements Control {
         }
 
         @Override
-        public Option getOption() {
+        public IntegerOption getOption() {
             return this.option;
         }
 
@@ -77,18 +76,18 @@ public class SliderControl implements Control {
 
             int labelWidth = this.font.width(label);
 
+            // render the label first and then the slider to prevent the highlight rect from darkening the slider
+            super.render(graphics, mouseX, mouseY, delta);
+
+            if (!this.option.showControl() || this.isResetOverlayActive()) {
+                return;
+            }
+
             boolean drawSlider = isEnabled && (this.hovered || this.isFocused());
             if (drawSlider) {
                 this.contentWidth = sliderWidth + labelWidth;
             } else {
                 this.contentWidth = labelWidth;
-            }
-
-            // render the label first and then the slider to prevent the highlight rect from darkening the slider
-            super.render(graphics, mouseX, mouseY, delta);
-
-            if (!this.option.showControl()) {
-                return;
             }
 
             if (drawSlider) {
@@ -153,6 +152,9 @@ public class SliderControl implements Control {
         @Override
         public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
             this.sliderHeld = false;
+
+            if (super.mouseClicked(event, doubleClick)) return true;
+            if (this.isResetOverlayActive()) return false;
 
             if (this.option.isEnabled() && event.button() == 0 && this.isMouseOver(event.x(), event.y())) {
                 if (this.isMouseOverSlider(event.x(), event.y())) {

common/src/main/java/net/caffeinemc/mods/sodium/client/gui/options/control/StatefulControlElement.java
@@ -1,0 +1,44 @@
+package net.caffeinemc.mods.sodium.client.gui.options.control;
+
+import net.caffeinemc.mods.sodium.client.config.structure.StatefulOption;
+import net.caffeinemc.mods.sodium.client.gui.ColorTheme;
+import net.caffeinemc.mods.sodium.client.gui.widgets.ResetButton;
+import net.caffeinemc.mods.sodium.client.util.Dim2i;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.input.MouseButtonEvent;
+
+public abstract class StatefulControlElement extends ControlElement {
+    protected final ResetButton resetButton;
+
+    public StatefulControlElement(AbstractOptionList list, Dim2i dim, ColorTheme theme) {
+        super(list, dim, theme);
+
+        this.resetButton = new ResetButton(this, () -> this.getOption().resetToDefault());
+    }
+
+    public boolean isResetOverlayActive() {
+        return this.resetButton.isActive();
+    }
+
+    public abstract StatefulOption<?> getOption();
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        this.hovered = this.isMouseOver(mouseX, mouseY);
+
+        super.render(graphics, mouseX, mouseY, delta);
+
+        this.resetButton.render(graphics, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+        return this.resetButton.mouseClicked(event, doubleClick);
+    }
+
+    @Override
+    protected String truncateLabelToFit(String name) {
+        int rightReserve = this.isResetOverlayActive() ? this.resetButton.getWidth() : this.getContentWidth() + 20;
+        return truncateTextToFit(name, this.getWidth() - rightReserve);
+    }
+}

common/src/main/java/net/caffeinemc/mods/sodium/client/gui/widgets/ResetButton.java
@@ -1,0 +1,89 @@
+package net.caffeinemc.mods.sodium.client.gui.widgets;
+
+import com.mojang.blaze3d.platform.cursor.CursorTypes;
+import net.caffeinemc.mods.sodium.client.gui.Layout;
+import net.caffeinemc.mods.sodium.client.util.Dim2i;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ComponentPath;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.FocusNavigationEvent;
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.Identifier;
+import org.jspecify.annotations.Nullable;
+
+/**
+ * Right-aligned reset overlay shown when the parent row is hovered while SHIFT is held.
+ * It derives is positioning from the parent so it tracks the parent's scroll position automatically.
+ */
+public class ResetButton extends AbstractWidget {
+    private static final Identifier ICON = Identifier.fromNamespaceAndPath("sodium", "textures/gui/reset_button.png");
+    private static final int ICON_SIZE = 10;
+    private static final int COLOR = 0xFFFF8C30;
+
+    private final AbstractWidget parent;
+    private final Runnable action;
+
+    public ResetButton(AbstractWidget parent, Runnable action) {
+        super(new Dim2i(0, 0, Layout.BUTTON_SHORT, 0));
+        this.parent = parent;
+        this.action = action;
+    }
+
+    public static boolean isShiftHeld() {
+        return Minecraft.getInstance().hasShiftDown();
+    }
+
+    public boolean isActive() {
+        return this.parent.isHovered() && isShiftHeld();
+    }
+
+    @Override
+    public int getX() {
+        return this.parent.getLimitX() - this.getWidth();
+    }
+
+    @Override
+    public int getY() {
+        return this.parent.getY();
+    }
+
+    @Override
+    public int getHeight() {
+        return this.parent.getHeight();
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        if (!this.isActive()) {
+            return;
+        }
+
+        int x = this.getCenterX() - ICON_SIZE / 2;
+        int y = this.getCenterY() - ICON_SIZE / 2;
+
+        graphics.blit(RenderPipelines.GUI_TEXTURED, ICON, x, y, 0, 0, ICON_SIZE, ICON_SIZE, ICON_SIZE, ICON_SIZE, COLOR);
+        graphics.requestCursor(CursorTypes.POINTING_HAND);
+    }
+
+    @Override
+    public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+        if (!isShiftHeld() || event.button() != 0) {
+            return false;
+        }
+
+        if (!this.parent.isMouseOver(event.x(), event.y()) || !this.isMouseOver(event.x(), event.y())) {
+            return false;
+        }
+
+        this.action.run();
+        this.playClickSound();
+        return true;
+    }
+
+    @Override
+    public @Nullable ComponentPath nextFocusPath(FocusNavigationEvent event) {
+        // Only reachable via SHIFT-hover + click.
+        return null;
+    }
+}