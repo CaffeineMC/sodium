f1327c0071673082820e694db9d478037b46fe49
douira <douira100@gmail.com>
2026-05-03 03:44:35 +0200

    Fix class loader rejecting paths that start with /

--- common/src/main/java/net/caffeinemc/mods/sodium/client/gl/shader/ShaderLoader.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/gl/shader/ShaderLoader.java
@@ -1,11 +1,11 @@
 package net.caffeinemc.mods.sodium.client.gl.shader;
 
+import net.minecraft.resources.ResourceLocation;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import net.minecraft.resources.ResourceLocation;
 
 public class ShaderLoader {
     /**
@@ -23,7 +23,7 @@ public class ShaderLoader {
     }
 
     public static String getShaderSource(ResourceLocation name) {
-        String path = String.format("/assets/%s/shaders/%s", name.getNamespace(), name.getPath());
+        String path = String.format("assets/%s/shaders/%s", name.getNamespace(), name.getPath());
 
         try (InputStream in = ShaderLoader.class.getClassLoader().getResourceAsStream(path)) {
             if (in == null) {
