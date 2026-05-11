9a706a5a4c00f3242b35982a3e32ac62a068bd28
douira <douira100@gmail.com>
2026-05-11 02:24:35 +0200

    Bump version to 0.8.12-beta.2

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -10,7 +10,7 @@ object BuildConfig {
     val PARCHMENT_VERSION: String? = "2024.11.17"
 
     // https://semver.org/
-    val MOD_VERSION: String = "0.8.11"
+    val MOD_VERSION: String = "0.8.12-beta.2"
 
     val RELEASE_TAG: String = "mc$MINECRAFT_VERSION-$MOD_VERSION"
 
