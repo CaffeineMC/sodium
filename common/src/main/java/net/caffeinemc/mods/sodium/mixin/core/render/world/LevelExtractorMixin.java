9ee8025a16ed53c2fde13da7d765c3a8456c9873
Moulberry <james.jenour@protonmail.com>
2026-06-25 02:39:29 +0800

    Use occlusion culling from camera render state (#3764)

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/core/render/world/LevelExtractorMixin.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/core/render/world/LevelExtractorMixin.java
@@ -16,10 +16,7 @@ import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jspecify.annotations.Nullable;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -31,6 +28,10 @@ import java.util.function.Consumer;
 public class LevelExtractorMixin {
     @Shadow
     private @Nullable ClientLevel level;
+
+    @Shadow
+    @Final
+    private LevelRenderState levelRenderState;
 
     @Unique
     private SodiumWorldRenderer renderer;
@@ -127,7 +128,8 @@ public class LevelExtractorMixin {
         var viewport = ((ViewportProvider) frustum).sodium$createViewport();
         var updateChunksImmediately = FlawlessFrames.isActive();
 
-        this.renderer.setupTerrain(camera, viewport, ((FogStorage) Minecraft.getInstance().gameRenderer).sodium$getFogParameters(), Minecraft.getInstance().player != null && Minecraft.getInstance().player.isSpectator(), updateChunksImmediately, ((FrustumAccessor) frustum).sodium$getMatrix());
+        boolean useOcclusionCulling = this.levelRenderState.cameraRenderState.smartCull;
+        this.renderer.setupTerrain(camera, viewport, ((FogStorage) Minecraft.getInstance().gameRenderer).sodium$getFogParameters(), useOcclusionCulling, updateChunksImmediately, ((FrustumAccessor) frustum).sodium$getMatrix());
     }
 
     @Redirect(method = "extract", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/extract/LevelExtractor;applyFrustum(Lnet/minecraft/client/renderer/culling/Frustum;)V"))
