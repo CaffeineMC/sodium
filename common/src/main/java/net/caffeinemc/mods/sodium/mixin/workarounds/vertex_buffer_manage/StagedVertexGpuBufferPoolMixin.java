9937ffe3f50aa3b8c69801625b3e75f8093f8a4c
GeForceLegend <41003546+GeForceLegend@users.noreply.github.com>
2026-07-26 23:58:08 +0800

    Removed available buffer clear at end of each frame

---
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/workarounds/vertex_buffer_manage/StagedVertexGpuBufferPoolMixin.java
@@ -0,0 +1,33 @@
+package net.caffeinemc.mods.sodium.mixin.workarounds.vertex_buffer_manage;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.blaze3d.buffers.GpuBuffer;
+import com.mojang.blaze3d.buffers.GpuFence;
+import com.mojang.blaze3d.systems.GpuDevice;
+import net.minecraft.client.renderer.StagedVertexBuffer;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+@Mixin(StagedVertexBuffer.GpuBufferPool.class)
+public class StagedVertexGpuBufferPoolMixin {
+    @Shadow
+    @Final
+    private List<GpuBuffer> usedThisFrame;
+
+    @Shadow
+    @Final
+    private List<StagedVertexBuffer.GpuBufferPool.PendingRecycle> pendingRecycle;
+
+    @WrapMethod(method = "endFrame")
+    private void iris$endFrame(GpuDevice device, Operation<Void> original) {
+        if (!this.usedThisFrame.isEmpty()) {
+            GpuFence fence = device.createCommandEncoder().createFence();
+            this.pendingRecycle.add(new StagedVertexBuffer.GpuBufferPool.PendingRecycle(List.copyOf(this.usedThisFrame), fence));
+            this.usedThisFrame.clear();
+        }
+    }
+}
