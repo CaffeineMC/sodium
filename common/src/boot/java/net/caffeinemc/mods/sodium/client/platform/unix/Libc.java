64cf2c316e9f713e49bda0e43230cd6cc99a634f
JellySquid <jellysquid@pm.me>
2026-07-27 00:49:50 +0200

    Use clearenv() to delete environment variables
    Calling setenv() with a NULL value is not permitted by
    spec, although nothing currently tries to do this. To make
    the function more consistent with the Win32 function, let's
    handle the NULL case.

--- common/src/boot/java/net/caffeinemc/mods/sodium/client/platform/unix/Libc.java
+++ common/src/boot/java/net/caffeinemc/mods/sodium/client/platform/unix/Libc.java
@@ -1,5 +1,6 @@
 package net.caffeinemc.mods.sodium.client.platform.unix;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.system.*;
 
@@ -7,17 +8,46 @@ public class Libc {
     private static final SharedLibrary LIBRARY = APIUtil.apiCreateLibrary("libc.so.6");
 
     private static final long PFN_setenv;
+    private static final long PFN_unsetenv;
 
     static {
         PFN_setenv = APIUtil.apiGetFunctionAddress(LIBRARY, "setenv");
+        PFN_unsetenv = APIUtil.apiGetFunctionAddress(LIBRARY, "unsetenv");
     }
 
     public static void setEnvironmentVariable(String name, @Nullable String value) {
-        try (var stack = MemoryStack.stackPush()) {
-            var nameBuf = stack.UTF8(name);
-            var valueBuf = value != null ? stack.UTF8(value) : null;
+        if (value != null) {
+            setenv(name, value);
+        } else {
+            unsetenv(name);
+        }
+    }
 
-            JNI.callPPI(MemoryUtil.memAddress(nameBuf), MemoryUtil.memAddressSafe(valueBuf), 1 /* replace */, PFN_setenv);
+    private static void setenv(String name, @NotNull String value) {
+        int result;
+
+        try (var stack = MemoryStack.stackPush()) {
+            result = JNI.callPPI(
+                    MemoryUtil.memAddress(stack.UTF8(name)),
+                    MemoryUtil.memAddress(stack.UTF8(value)),
+                    1 /* replace */,
+                    PFN_setenv);
+        }
+
+        if (result != 0) {
+            throw new RuntimeException("setenv() failed: %d".formatted(result));
+        }
+    }
+
+    private static void unsetenv(@NotNull String name) {
+        int result;
+
+        try (var stack = MemoryStack.stackPush()) {
+            result = JNI.callPI(MemoryUtil.memAddress(stack.UTF8(name)), PFN_unsetenv);
+        }
+
+        if (result != 0) {
+            throw new RuntimeException("unsetenv() failed: %d".formatted(result));
         }
     }
 }
