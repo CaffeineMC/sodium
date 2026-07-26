IMS212/sodium-fabric

Implementation of a defragmenting auto-sizing buffer arena
always check segment assertions for now, potentially improve arena sizing

fix free list corruption bug

work more on compaction, disable for now as it doesn't work well

bug fixes, correct owner change logic, improved deallocation heuristics, improved arena sizing

compact smallest arena into free space if possible, new arena sizing heuristics

prevent oscillation when emptying arenas by not allowing new allocation during emptying-related eviction

properly handle migrating zero-usage regions, and fix bug by not deleting the only existing arena if there's only one

deallocate arenas when there's enough margin of usable free space in other arenas to evict its contents

update only the segments that changed when defragmenting

use 32mib index arena by default

improve heuristics to reduce defrag effort when not needed

refactor defrag code for moving free spaces towards each other

limit copy and byte budget for defragmenting

renames for clarity

improve fragmentation measure, show total defrag copies and volume, modulate brightness of visualizer segments

defragment towards second biggest segment to reduce number of copies and copied bytes

Incremental defragmentation

buffer visualization

detect best shared arena to place region correctly

notes on defragmentation

split free tracking into a separate class

use multiple shared buffers

rename to shared arena

TreeMap has been profiled to be much faster

fix bugs, improve names

more assertion

use tree list to track free segments

fix crashes when f3 is open and arenas are gone

young gen buffer handling

migrate gl buffer arena to be generated by a central instance to make buffer sharing more elegant and not leak shared buffers

common/src/main/java/net/caffeinemc/mods/sodium/client/gl/arena/AllocatorBase.java
@@ -1,0 +1,15 @@
+package net.caffeinemc.mods.sodium.client.gl.arena;
+
+import net.caffeinemc.mods.sodium.client.gl.buffer.GlBuffer;
+
+public interface AllocatorBase {
+    long getDeviceUsedMemory();
+
+    long getDeviceAllocatedMemory();
+
+    void free(GlBufferSegment entry);
+
+    boolean isEmpty();
+
+    GlBuffer getBufferObject();
+}

common/src/main/java/net/caffeinemc/mods/sodium/client/gl/arena/RegionAllocatorHandle.java
@@ -1,0 +1,102 @@
+package net.caffeinemc.mods.sodium.client.gl.arena;
+
+import net.caffeinemc.mods.sodium.client.gl.buffer.GlBuffer;
+import net.caffeinemc.mods.sodium.client.gl.device.CommandList;
+import net.caffeinemc.mods.sodium.client.render.chunk.region.RenderRegion;
+
+import java.util.stream.Stream;
+
+public class RegionAllocatorHandle implements AllocatorBase, SizedTreeMap.Sized {
+    private final RenderRegion region;
+    private final AllocationChangeConsumer onChange;
+    private GlBufferArena backingArena;
+    long used;
+    int usedSegments;
+    int identifier;
+    private static int nextIdentifier = 1;
+
+    public RegionAllocatorHandle(RenderRegion region, AllocationChangeConsumer onChange, GlBufferArena backingArena) {
+        this.region = region;
+        this.onChange = onChange;
+        this.backingArena = backingArena;
+        this.identifier = nextIdentifier++;
+
+        this.backingArena.registerOwner(this);
+    }
+
+    public interface AllocationChangeConsumer {
+        void onBufferChanged(CommandList commandList);
+
+        void onSegmentChanged(CommandList commandList, int ownerIndex);
+    }
+
+    float getFillFractionInv() {
+        return this.region.getFillFractionInv();
+    }
+
+    void setBackingArena(GlBufferArena arena) {
+        this.backingArena = arena;
+    }
+
+    @Override
+    public long getDeviceAllocatedMemory() {
+        return this.backingArena.getDeviceAllocatedMemory();
+    }
+
+    @Override
+    public long getDeviceUsedMemory() {
+        return this.backingArena.getDeviceUsedMemory();
+    }
+
+    @Override
+    public void free(GlBufferSegment entry) {
+        this.backingArena.free(entry);
+    }
+
+    public void deleteSingleOwner(CommandList commands) {
+        // differentiation of single-owner or shared deletion is handled at the arena level
+        this.backingArena.deleteSingleOwner(commands, this);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.backingArena.isEmpty();
+    }
+
+    @Override
+    public GlBuffer getBufferObject() {
+        return this.backingArena.getBufferObject();
+    }
+
+    public boolean upload(CommandList commandList, Stream<PendingUpload> stream) {
+        var prevBackingArena = this.backingArena;
+        var bufferChanged = this.backingArena.upload(commandList, this, stream);
+        return bufferChanged || this.backingArena != prevBackingArena;
+    }
+
+    public GlBufferArena getBackingArena() {
+        return this.backingArena;
+    }
+
+    public boolean isSingleOwner() {
+        return !(this.backingArena instanceof SharedGlBufferArena);
+    }
+
+    public void notifyBufferChanged(CommandList commandList) {
+        this.onChange.onBufferChanged(commandList);
+    }
+
+    public void notifySegmentChanged(CommandList commandList, int ownerIndex) {
+        this.onChange.onSegmentChanged(commandList, ownerIndex);
+    }
+
+    @Override
+    public long getSize() {
+        return this.used;
+    }
+
+    @Override
+    public long getIdentifier() {
+        return this.identifier;
+    }
+}

CaffeineMC/sodium

common/src/main/java/net/caffeinemc/mods/sodium/client/gl/arena/AllocatorBase.java
@@ -1,0 +1,15 @@
+package net.caffeinemc.mods.sodium.client.gl.arena;
+
+import net.caffeinemc.mods.sodium.client.gl.buffer.GlBuffer;
+
+public interface AllocatorBase {
+    long getDeviceUsedMemory();
+
+    long getDeviceAllocatedMemory();
+
+    void free(BufferSegment entry);
+
+    boolean isEmpty();
+
+    GlBuffer getBufferObject();
+}

common/src/main/java/net/caffeinemc/mods/sodium/client/gl/arena/RegionAllocatorHandle.java
@@ -1,0 +1,102 @@
+package net.caffeinemc.mods.sodium.client.gl.arena;
+
+import net.caffeinemc.mods.sodium.client.gl.buffer.GlBuffer;
+import net.caffeinemc.mods.sodium.client.gl.device.CommandList;
+import net.caffeinemc.mods.sodium.client.render.chunk.region.RenderRegion;
+
+import java.util.stream.Stream;
+
+public class RegionAllocatorHandle implements AllocatorBase, SizedTreeMap.Sized {
+    private final RenderRegion region;
+    private final AllocationChangeConsumer onChange;
+    private BufferArena backingArena;
+    long used;
+    int usedSegments;
+    int identifier;
+    private static int nextIdentifier = 1;
+
+    public RegionAllocatorHandle(RenderRegion region, AllocationChangeConsumer onChange, BufferArena backingArena) {
+        this.region = region;
+        this.onChange = onChange;
+        this.backingArena = backingArena;
+        this.identifier = nextIdentifier++;
+
+        this.backingArena.registerOwner(this);
+    }
+
+    public interface AllocationChangeConsumer {
+        void onBufferChanged(CommandList commandList);
+
+        void onSegmentChanged(CommandList commandList, int ownerIndex);
+    }
+
+    float getFillFractionInv() {
+        return this.region.getFillFractionInv();
+    }
+
+    void setBackingArena(BufferArena arena) {
+        this.backingArena = arena;
+    }
+
+    @Override
+    public long getDeviceAllocatedMemory() {
+        return this.backingArena.getDeviceAllocatedMemory();
+    }
+
+    @Override
+    public long getDeviceUsedMemory() {
+        return this.backingArena.getDeviceUsedMemory();
+    }
+
+    @Override
+    public void free(BufferSegment entry) {
+        this.backingArena.free(entry);
+    }
+
+    public void deleteSingleOwner(CommandList commands) {
+        // differentiation of single-owner or shared deletion is handled at the arena level
+        this.backingArena.deleteSingleOwner(commands, this);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.backingArena.isEmpty();
+    }
+
+    @Override
+    public GlBuffer getBufferObject() {
+        return this.backingArena.getBufferObject();
+    }
+
+    public boolean upload(CommandList commandList, Stream<PendingUpload> stream) {
+        var prevBackingArena = this.backingArena;
+        var bufferChanged = this.backingArena.upload(commandList, this, stream);
+        return bufferChanged || this.backingArena != prevBackingArena;
+    }
+
+    public BufferArena getBackingArena() {
+        return this.backingArena;
+    }
+
+    public boolean isSingleOwner() {
+        return !(this.backingArena instanceof SharedBufferArena);
+    }
+
+    public void notifyBufferChanged(CommandList commandList) {
+        this.onChange.onBufferChanged(commandList);
+    }
+
+    public void notifySegmentChanged(CommandList commandList, int ownerIndex) {
+        this.onChange.onSegmentChanged(commandList, ownerIndex);
+    }
+
+    @Override
+    public long getSize() {
+        return this.used;
+    }
+
+    @Override
+    public long getIdentifier() {
+        return this.identifier;
+    }
+}