448584f4c7adf2d8fb9ab4140998f47e7901066b
IMS212 <ims@irisshaders.dev>
2026-07-31 07:44:27 -0700

    Handle resizing the section time buffer (26.1)

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/UniformBufferManager.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/UniformBufferManager.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.buffers.GpuBuffer;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.buffers.Std140Builder;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.caffeinemc.mods.sodium.client.SodiumClientMod;
 import net.caffeinemc.mods.sodium.client.gl.arena.staging.MappedStagingBuffer;
 import net.caffeinemc.mods.sodium.client.gl.buffer.GlBufferTarget;
 import net.caffeinemc.mods.sodium.client.gl.buffer.GlTexelBuffer;
@@ -34,9 +35,9 @@ public class UniformBufferManager {
     private final DynamicUniformStorage<GlobalUniforms> uniformStorage;
     private GpuBufferSlice uniformData;
 
-    private final GpuBuffer sectionTimeInfo;
-    private final GlTexelBuffer sectionTimeInfoTexture;
-    private final GpuBuffer.MappedView sectionTimeInfoMap;
+    private GpuBuffer sectionTimeInfo;
+    private GlTexelBuffer sectionTimeInfoTexture;
+    private GpuBuffer.MappedView sectionTimeInfoMap;
 
     private boolean hasUpdatedThisFrame = false;
 
@@ -52,22 +53,53 @@ public class UniformBufferManager {
         this.uniformStorage = new DynamicUniformStorage<>("Sodium terrain uniforms", GLOBAL_UNIFORM_SIZE, INITIAL_GLOBAL_UNIFORM_CAPACITY);
 
         this.sectionTimeInfo = RenderSystem.getDevice().createBuffer(() -> "Section time info",
-                GpuBuffer.USAGE_UNIFORM_TEXEL_BUFFER | GpuBuffer.USAGE_COPY_DST | GpuBuffer.USAGE_MAP_WRITE,
+                GpuBuffer.USAGE_UNIFORM_TEXEL_BUFFER | GpuBuffer.USAGE_COPY_SRC | GpuBuffer.USAGE_COPY_DST | GpuBuffer.USAGE_MAP_WRITE,
                 (long) maxRegions * TIME_BUFFER_SIZE_PER_REGION);
 
         this.sectionTimeInfoTexture = new GlTexelBuffer(((GlBufferAccessor) this.sectionTimeInfo).sodium$getHandle(), GL46C.GL_R32I);
 
-        ByteBuffer copy = MemoryUtil.memAlloc(maxRegions * TIME_BUFFER_SIZE_PER_REGION);
-        MemoryUtil.memSet(copy, 0xFFFFFFFF);
-        RenderSystem.getDevice().createCommandEncoder().writeToBuffer(this.sectionTimeInfo.slice(), copy);
+        if (MappedStagingBuffer.isSupported(RenderDevice.INSTANCE)) {
+            this.sectionTimeInfoMap = RenderSystem.getDevice().createCommandEncoder().mapBuffer(this.sectionTimeInfo, false, true);
+            MemoryUtil.memSet(sectionTimeInfoMap.data(), 0xFFFFFFFF);
+        } else {
+            try (var map = RenderSystem.getDevice().createCommandEncoder().mapBuffer(this.sectionTimeInfo, false, true)) {
+                MemoryUtil.memSet(map.data(), 0xFFFFFFFF);
+            }
+            this.sectionTimeInfoMap = null;
+        }
+
         onSectionTimeInfoWritten();
-        MemoryUtil.memFree(copy);
+    }
+
+    // Somehow, some mods cause more regions to be loaded than are mathematically possible. This fixes that case, while logging it happened.
+    public void resizeIfNeeded(int newRequiredIndex) {
+        var newSize = Math.max(this.sectionTimeInfo.size() * 2, (long) newRequiredIndex * TIME_BUFFER_SIZE_PER_REGION);
+
+        // Note that either something has gone wrong, or a mod is interfering weirdly
+        SodiumClientMod.logger().warn("Had to resize the section time buffer to be bigger ({} bytes), this should never be possible in Vanilla.", newSize);
+
+        if (sectionTimeInfoMap != null) sectionTimeInfoMap.close();
+        if (sectionTimeInfoTexture != null) sectionTimeInfoTexture.destroy();
+
+        var oldBuffer = sectionTimeInfo;
+        sectionTimeInfo = RenderSystem.getDevice().createBuffer(() -> "Section time info", GpuBuffer.USAGE_UNIFORM_TEXEL_BUFFER | GpuBuffer.USAGE_COPY_SRC | GpuBuffer.USAGE_COPY_DST | GpuBuffer.USAGE_MAP_WRITE, newSize);
+
+        RenderSystem.getDevice().createCommandEncoder().copyToBuffer(oldBuffer.slice(), sectionTimeInfo.slice(0, oldBuffer.size()));
 
         if (MappedStagingBuffer.isSupported(RenderDevice.INSTANCE)) {
             this.sectionTimeInfoMap = RenderSystem.getDevice().createCommandEncoder().mapBuffer(this.sectionTimeInfo, false, true);
+            MemoryUtil.memSet(MemoryUtil.memAddress(sectionTimeInfoMap.data()) + oldBuffer.size(), 0xFFFFFFFF, sectionTimeInfo.size() - oldBuffer.size());
         } else {
-            this.sectionTimeInfoMap = null;
+            try (var mapping = RenderSystem.getDevice().createCommandEncoder().mapBuffer(this.sectionTimeInfo, false, true)) {
+                MemoryUtil.memSet(MemoryUtil.memAddress(mapping.data()) + oldBuffer.size(), 0xFFFFFFFF, sectionTimeInfo.size() - oldBuffer.size());
+            }
         }
+
+        this.sectionTimeInfoTexture = new GlTexelBuffer(((GlBufferAccessor) this.sectionTimeInfo).sodium$getHandle(), GL46C.GL_R32I);
+
+        oldBuffer.close(); // defers
+
+        onSectionTimeInfoWritten();
     }
 
     // When we bind a buffer to this target because we're writing without using a mapped buffer, then we're modifying the state without GlStateTracker being part of the process. This method notifies it so it doesn't skip re-binding the buffer later after the state has become invalid.
@@ -150,7 +182,7 @@ public class UniformBufferManager {
         long sectionTimeOffset = ((long) id * RenderRegion.REGION_SIZE + sectionIndex) * Integer.BYTES;
 
         if (sectionTimeOffset >= this.sectionTimeInfo.size()) {
-            throw new IllegalStateException("Overflowed the mesh time buffer at " + id + "x" + sectionIndex);
+            resizeIfNeeded(id);
         }
 
         if (this.sectionTimeInfoMap != null) {
