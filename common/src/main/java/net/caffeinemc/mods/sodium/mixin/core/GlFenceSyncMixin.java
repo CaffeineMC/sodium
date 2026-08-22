617540a2178cf3d2dc4ae8775212ad0c43aa6697
kupa22 <58780090+kupa22@users.noreply.github.com>
2026-08-22 23:43:57 +0200

    Fix OpenGL performance regression introduced by glFenceSync

---
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/core/GlFenceSyncMixin.java
@@ -0,0 +1,21 @@
+package net.caffeinemc.mods.sodium.mixin.core;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(targets = "com.mojang.blaze3d.opengl.GlCommandEncoder")
+public class GlFenceSyncMixin {
+
+    @Redirect(
+            method = "submit",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/lwjgl/opengl/GL33C;glFenceSync(II)J"
+            )
+    )
+    private long sodium$disableGlFenceSync(int condition, int flags) {
+        return 0L;
+    }
+
+}
